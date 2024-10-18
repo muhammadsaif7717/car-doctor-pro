d8fbcd48a14880a8127161416ace084ae74fcad1
Muhammad Saif <muhammadsaif7717@gail.com>
2024-10-18 12:43:56 -0700

    Banner Added

--- src/components/HomePage/Banner.jsx
+++ src/components/HomePage/Banner.jsx
@@ -7,6 +7,8 @@ const Banner = () => {
             number: 1,
             src: '/assets/images/banner/1.jpg',
             alt: 'banner1',
+            title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, officia.',
+            desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti, a sapiente dolor nihil ullam quae quibusdam perspiciatis possimus est ad atque exercitationem? Quam, nesciunt.',
             prev: '#5',
             next: '#2',
         },
@@ -14,6 +16,8 @@ const Banner = () => {
             number: 2,
             src: '/assets/images/banner/2.jpg',
             alt: 'banner2',
+            title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, officia.',
+            desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti, a sapiente dolor nihil ullam quae quibusdam perspiciatis possimus est ad atque exercitationem? Quam, nesciunt.',
             prev: '#1',
             next: '#3',
         },
@@ -21,6 +25,8 @@ const Banner = () => {
             number: 3,
             src: '/assets/images/banner/3.jpg',
             alt: 'banner3',
+            title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, officia.',
+            desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti, a sapiente dolor nihil ullam quae quibusdam perspiciatis possimus est ad atque exercitationem? Quam, nesciunt.',
             prev: '#2',
             next: '#4',
         },
@@ -28,6 +34,8 @@ const Banner = () => {
             number: 4,
             src: '/assets/images/banner/4.jpg',
             alt: 'banner4',
+            title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, officia.',
+            desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti, a sapiente dolor nihil ullam quae quibusdam perspiciatis possimus est ad atque exercitationem? Quam, nesciunt.',
             prev: '#3',
             next: '#5',
         },
@@ -35,6 +43,8 @@ const Banner = () => {
             number: 5,
             src: '/assets/images/banner/5.jpg',
             alt: 'banner5',
+            title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, officia.',
+            desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti, a sapiente dolor nihil ullam quae quibusdam perspiciatis possimus est ad atque exercitationem? Quam, nesciunt.',
             prev: '#4',
             next: '#1',
         },
@@ -44,9 +54,14 @@ const Banner = () => {
             <div className="carousel w-full">
                 {
                     slides.map((slide) => (
-                        <div key={slide.number} id={slide.number} className="carousel-item relative w-full">
+                        <div key={slide.number} id={slide.number} className="carousel-item relative w-full h-[700px]">
                             <Image src={slide.src} width={1080} height={1080} alt={slide.alt}
-                                className="w-full" />
+                                className="w-full h-full brightness-75" />
+                            <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white'>
+                                <h1>{slide.title}</h1>
+                                <p>{slide.desc}</p>
+                            </div>
+
                             <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
                                 <a href={slide.prev} className="btn btn-circle">❮</a>
                                 <a href={slide.next} className="btn btn-circle">❯</a>
--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -17,9 +17,9 @@ const Navbar = () => {
     </div>
     return (
         <div className='flex justify-center  '>
-            <div className="navbar flex justify-evenly bg-base-100  bg-opacity-80 fixed max-w-screen-xl mx-auto z-50 ">
+            <div className="navbar flex justify-evenly bg-base-100  bg-opacity-80 fixed max-w-screen-xl mx-auto z-50 py-2">
                 <div className="navbar-start">
-                    <div className="dropdown">
+                    <div className="dropdown items-center">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
